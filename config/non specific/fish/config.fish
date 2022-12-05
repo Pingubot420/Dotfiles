9acbba7f2bf0c2c8398dc3fb3b3bf12068879578
PinguBot420 <65819706+Pingubot420@users.noreply.github.com>
2022-12-05 21:57:24 +0200

    fish
    blub blub

--- config/non specific/fish/config.fish
+++ config/non specific/fish/config.fish
@@ -1,3 +1,15 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function fish_greeting
+    echo The time is (set_color purple; date +%T; set_color purple)
 end
+alias ls="exa -l -a --group-directories-first --icons"
+alias gc="git clone"
+
+function timeis
+    echo The time is (set_color purple; date +%T; set_color purple)
+end
+
+export PATH="~/.local/bin:$PATH"
+alias vim="nvim"
+alias transcat="queercat -b -f 1 -v 0.45 -h 0.45"
+alias clock="tty-clock -s -C 5 -D -c -b"
+starship init fish | source
--- config/non specific/fish/functions/fish_prompt.fish
+++ config/non specific/fish/functions/fish_prompt.fish
@@ -1,98 +1,33 @@
- function fish_prompt
-  # Cache exit status
-  set -l last_status $status
+function fish_prompt
+	# Store the exit code of the last command
+	set -g sf_exit_code $status
+	set -g SPACEFISH_VERSION 2.7.0
 
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (uname -n|cut -d . -f 1)
-  end
-  if not set -q __fish_prompt_char
-    switch (id -u)
-      case 0
-        set -g __fish_prompt_char '#'
-      case '*'
-        set -g __fish_prompt_char 'λ'
-    end
-  end
+	# ------------------------------------------------------------------------------
+	# Configuration
+	# ------------------------------------------------------------------------------
 
-  # Setup colors
-  set -l hostcolor (set_color (uname -n | md5sum | cut -f1 -d' ' | tr -d '\n' | tail -c6))
-  set -l normal (set_color normal)
-  set -l white (set_color FFFFFF)
-  set -l turquoise (set_color 5fdfff)
-  set -l orange (set_color df5f00)
-  set -l hotpink (set_color df005f)
-  set -l blue (set_color blue)
-  set -l limegreen (set_color 87ff00)
-  set -l purple (set_color af5fff)
+	__sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
+	__sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
+	__sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
+	__sf_util_set_default SPACEFISH_PROMPT_ORDER time user dir host git package node ruby golang php rust haskell julia elixir docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery vi_mode jobs exit_code char
 
-  # Configure __fish_git_prompt
-  set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color 5fdfff
-  set -g __fish_git_prompt_color_flags df5f00
-  set -g __fish_git_prompt_color_prefix white
-  set -g __fish_git_prompt_color_suffix white
-  set -g __fish_git_prompt_showdirtystate true
-  set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_show_informative_status true
+	# ------------------------------------------------------------------------------
+	# Sections
+	# ------------------------------------------------------------------------------
 
-  set -l current_user (whoami)
+	# Keep track of whether the prompt has already been opened
+	set -g sf_prompt_opened $SPACEFISH_PROMPT_FIRST_PREFIX_SHOW
 
-  ##
-  ## Line 1
-  ##
-  echo -n $hotpink'╭─'$hotpink$current_user$white' at '$blue$__fish_prompt_hostname$white' in '$turquoise(pwd|sed "s=$HOME=~=")$turquoise
-  __fish_git_prompt " (%s)"
-  echo
+	if test "$SPACEFISH_PROMPT_ADD_NEWLINE" = "true"
+		echo
+	end
 
-  ##
-  ## Line 2
-  ##
-  echo -n $hotpink'╰'
-
-  # Disable virtualenv's default prompt
-  set -g VIRTUAL_ENV_DISABLE_PROMPT true
-
-  # support for virtual env name
-  if set -q VIRTUAL_ENV
-    echo -n "($turquoise"(basename "$VIRTUAL_ENV")"$white)"
-  end
-
-  ##
-  ## Support for vi mode
-  ##
-  set -l lambdaViMode "$THEME_LAMBDA_VI_MODE"
-
-  # Do nothing if not in vi mode
-  if test "$fish_key_bindings" = fish_vi_key_bindings
-      or test "$fish_key_bindings" = fish_hybrid_key_bindings
-    if test -z (string match -ri '^no|false|0$' $lambdaViMode)
-      set_color --bold
-      echo -n $white'─['
-      switch $fish_bind_mode
-        case default
-          set_color red
-          echo -n 'n'
-        case insert
-          set_color green
-          echo -n 'i'
-        case replace_one
-          set_color green
-          echo -n 'r'
-        case replace
-          set_color cyan
-          echo -n 'r'
-        case visual
-          set_color magenta
-          echo -n 'v'
-      end
-      echo -n $white']'
-    end
-  end
-
-  ##
-  ## Rest of the prompt
-  ##
-  echo -n $hotpink'─'$white$__fish_prompt_char $normal
+	for i in $SPACEFISH_PROMPT_ORDER
+		eval __sf_section_$i
+	end
+	set_color normal
 end
